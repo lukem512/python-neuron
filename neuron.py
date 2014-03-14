46dc9ed80c7507fffb8ea9e797e5145fe2223a67
Lukem512 <lukem95@gmail.com>
2014-03-14 19:00:40 +0000

    Refactored

--- neuron.py
+++ neuron.py
@@ -6,16 +6,15 @@ class neuron:
     self.V = V
 
   # excite the neuron
-  def excite(self, t_m, E_L, V_reset, V_th, R_m, I_e, DT):
-    # dV/dt = 1/t_m (E_L - V + R_m . I_e)
-    dV =  DT * (1/t_m * (E_L - self.V + (R_m * I_e)))
+  def excite(self, t_m, E_L, V_reset, V_th, R_m, I_e, dT):
+    dV =  dT * (1/t_m * (E_L - self.V + (R_m * I_e)))
     self.V = self.V + dV
 
     # if V > V_th then V = V_reset
     if self.V > V_th:
       self.V = V_reset
 
-    print self.V
+    return self.V
        
 
 def main():
@@ -25,16 +24,16 @@ def main():
   V_th = -40.0     # mV
   R_m = 10.0       # Mohm
   I_e = 3.1        # nA
-  DT = 1.0         # ms
+  dT = 1.0         # ms
   
   ### Part 1 ###
   n1 = neuron(V_reset)
 
   # simulate for 1s
   T = 0
-  while T < 100:
-    n1.excite(t_m, E_L, V_reset, V_th, R_m, I_e, DT)
-    T = T + DT
+  while T < 1000:
+    print n1.excite(t_m, E_L, V_reset, V_th, R_m, I_e, dT)
+    T = T + dT
   
 
 if __name__ == "__main__":
